e9fbf5ec5edc6640e0029b5f36914de39004fb24
Gergely Nagy <gergely.nagy1205@gmail.com>
2019-07-05 13:28:32 +0200

    final version

--- week-02/day-1/src/Anagram.java
+++ week-02/day-1/src/Anagram.java
@@ -1,18 +1,34 @@
-/*public class Anagram {
+public class Anagram {
   public static void main(String[] args) {
     String input1 = "dog";
     String input2 = "god";
+
+    System.out.println(isAnagram(input1,input2));
   }
   public static boolean isAnagram(String input1, String input2){
-    boolean anagram = false;
-    if(input1.equals(input2)){
-      for(int i = 0; i < input1.length(); i++){
-        for(int j = 0; j < input1.length(); j++){
-          if(input1.charAt(i)==input2.charAt(j)){
-            anagram = true;
-          }
-        }
-      }
-    }
+    boolean anagramOrNot = false;
+    int input1Counter = 0;
+    int input2Counter = 0;
+    if(input1.length()==input2.length()){
+     for (int i = 0; i < input1.length(); i++){
+       input1Counter*=0;
+       input2Counter*=0;
+       for (int j = 0; j < input2.length(); j++){
+         if(input1.charAt(i)==input1.charAt(j)){
+           input1Counter++;
+         }
+         if(input1.charAt(i)==input2.charAt(j)){
+           input2Counter++;
+         }
+       }
+       if (input1Counter == input2Counter) {
+         anagramOrNot = true;
+       }
+       else {
+         return false;
+       }
+     }
+   }
+   return anagramOrNot;
   }
-}*/
+}
